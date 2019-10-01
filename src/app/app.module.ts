bc10d1f3740a237d969fe3ca75de4d045f0be939
Hiirome <gael.coudurier-curveur@ynov.com>
2019-10-01 11:31:06 +0200

    initial setups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NotFoundComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
